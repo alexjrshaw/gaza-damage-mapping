d71127ef438a61fc2f180840e8498b6b8b38951f
Alex Shaw <s1214882@ed.ac.uk>
2026-07-20 15:58:41 +0100

    Remove doc/ (Ukraine images); update LICENSE, setup.py, pyproject.toml, setup.cfg for Gaza fork

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="1.0.0",
-    author="Olivier Dietrich",
-    author_email="odietrich@ethz.ch",
-    description="Detecting building destruction in Ukraine using pixel-wise Sentine-1 amplitude time series",
-    url="https://github.com/olidietrich/ukraine-damage-mapping-tool",
+    author="Alex Shaw",
+    author_email="alexjrshaw@gmail.com",
+    description="Building-damage detection generalises across conflicts without retraining",
+    url="https://github.com/alexjrshaw/gaza-damage-mapping",
     packages=find_packages(),
-    test_suite="src.tests.test_all.suite",
 )
